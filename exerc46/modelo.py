e4224cd4f0614a607478777f6d7dfca4bb9e7367
LuanaChiodini <lchiodini87@gmail.com>
2019-07-04 21:12:40 -0300

    04/07

--- exerc46/modelo.py
+++ exerc46/modelo.py
@@ -33,8 +33,13 @@ if __name__ == "__main__":
 		print("erro")
 
 	o1 = Ocorrencia.create(ocorrencia="dor de cabeça")
-	r1 = Registro.create(data="2019-06-29", ocorrencia=o1, intensidade=0, observacao="jantei muito na noite anterior(resto de marmita do almoço)")
-	print("teste ocorrência 1")
+	o2 = Ocorrencia.create(ocorrencia="desanimo")
+	r1 = Registro.create(data="2019-06-29", ocorrencia=o1, intensidade=0, observacao="jantei muito na noite anterior")
+	r2 = Registro.create(data="2019-06-02", ocorrencia=o2, intensidade=1, observacao="desentendimento matianal")
+	r3 = Registro.create(data="2019-06-09", ocorrencia=o1, intensidade=0, observacao="jantei na véspera")
+	r4 = Registro.create(data="2019-06-01", ocorrencia=o2, intensidade=0, observacao="desânimo depois do almoço")
+	r5 = Registro.create(data="2019-05-31", ocorrencia=o1, intensidade=1, observacao="janta pesada na noite anterior")
+	print("ocorrrências")
 
 	todas = Registro.select()
 	for i in todas:
